ae2b1f06d046d812a263f27872385ad3e88fc9c9
QzwCompany <qizewei@vip.qq.com>
2017-05-24 16:26:28 +0800

    添加Simple

--- app/src/main/java/com/example/god/colortest/MainActivity.java
+++ app/src/main/java/com/example/god/colortest/MainActivity.java
@@ -2,29 +2,41 @@ package com.example.god.colortest;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.example.colorutils.GodUtils;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private TextView text;
+    private int backColor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        TextView text1 = (TextView)findViewById(R.id.colorText1);
-        TextView text2 = (TextView)findViewById(R.id.colorText2);
-        TextView text3 = (TextView)findViewById(R.id.colorText3);
-        String color = "55955522";
+        text = (TextView)findViewById(R.id.colorText1);
+        backColor = GodUtils.StringTransInt("ff009507");
+        text.setBackgroundColor(backColor);
+        Button dark = (Button)findViewById(R.id.darkButton);
+        Button light = (Button)findViewById(R.id.lightButton);
+        dark.setOnClickListener(this);
+        light.setOnClickListener(this);
+    }
 
-        int i = GodUtils.StringTransInt(color);
-        int i1 = GodUtils.LightAlpha(i,50);
-        text1.setBackgroundColor(i);
-        text2.setBackgroundColor(i1);
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.darkButton:
+                text.setBackgroundColor(GodUtils.TranslateDark(backColor,30));
+                break;
+            case R.id.lightButton:
+                text.setBackgroundColor(GodUtils.TranslateLight(backColor,30));
+                break;
 
-
-
-
+        }
 
     }
 }
